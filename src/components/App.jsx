05118619ba179c4fc0a85da9609fd68f792bb92f
Oleksandr Vasylenko <79399963+aalexblade@users.noreply.github.com>
2023-02-01 21:34:20 +0200

    DidMoun/DidUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,25 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+     if (parsedContacts) {
+      this.setState({
+        contacts: parsedContacts,
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) { 
+    if (
+      prevState.contacts.lenght !== 0 &&
+      prevState.contacts.lenght !== this.state.contacts.length
+    ) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  };
+
   formSubmitHandler = ({ name, number }) => {
     const { contacts } = this.state;
     let newContact = {
